a3e796ce4608e3210954e23136fb1008d7370ac5
CHAN <chanho2547@gmail.com>
2025-02-06 19:50:04 +0900

    make a button&input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white w-full max-w-screen-sm shadow-lg p-5 rounded-3xl "></div>
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white w-full max-w-screen-sm shadow-lg p-5 rounded-3xl flex flex-col gap-2">
+        <input
+          className="w-full rounded-full h-12  bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+          type="text"
+          placeholder="Serch here..."
+        />
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium  outline-none ">
+          Search
+        </button>
+      </div>
     </main>
   );
 }
