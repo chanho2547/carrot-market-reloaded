f465ce72b4678afcc778708d43174c0d82a8d352
CHAN <chanho2547@gmail.com>
2025-02-07 10:40:25 +0900

    테일윈드 프리티어 적용및 vaild 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-    <main className="bg-gray-900 sm:bg-red-300 h-screen flex items-center justify-center p-5">
+    <main className="bg-gray-900  h-screen flex items-center justify-center p-5">
       <div className="bg-white w-full max-w-screen-sm shadow-lg p-5 rounded-3xl flex flex-col md:flex-row gap-2">
         <input
-          className="w-full rounded-full h-12  bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="text"
-          placeholder="Serch here..."
+          className="w-full rounded-full h-12  bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          type="email"
+          required
+          placeholder="Email address"
         />
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium  outline-none md:px-10">
-          Search
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium  outline-none md:px-10 peer-invalid:bg-red-100 peer-required:bg-green-500">
+          Login
         </button>
       </div>
     </main>
